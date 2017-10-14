00c5f98896e110ce0ad8c1e89c0c141a636f4286
Joel Holdsworth <joel@airwebreathe.org.uk>
2017-10-13 18:48:12 -0600

    match.c: Optimized find_match algorithm
    This patch replaces the current implementation of find_match with
    a faster implementation.
    
    The faster implementation works by precomputing the match
    conditions as a series of masks that match logic-high, low, change
    and no-change. Because the match-terms are now combined into words,
    there is now bit/word level parallelism; all the terms of a single
    condition are combined in a single set of masks, which can be
    matched to samples in all at once.
    
    Further speed improvements are gained by...
     * ...eliminating linked-list traversal from the inner loop. The
       masks are stored contigiously within an array to maintain good
       cache coherence.
     * ...eliminating reduntant counters. Previously a loop-counter,
       abs_cur_samplenum and the term skip counters were incremented
       within the loop. These are now represented by a single
       incrementing pointer sample_pos. Other counters are updated
       once when the loop has finished.
     * ...removing sample position calculation in every loop. Instead
       the sample position pointerr is calculated once, and incremented
       by the unit-size every loop.
     * ...taking advantage of unaligned access, where available, to
       read whole sample words at once rather than loading words piece-
       wise in single bytes.

--- match.c
+++ match.c
@@ -24,64 +24,6 @@
 #include <glib.h>
 #include <stdint.h>
 
-/**
- * Check whether the specified sample matches the specified term.
- *
- * In the case of SRD_TERM_SKIP, this function can modify
- * term->num_samples_already_skipped.
- *
- * @param old_sample The value of the previous sample (0/1).
- * @param sample The value of the current sample (0/1).
- * @param term The term that should be checked for a match. Must not be NULL.
- *
- * @retval TRUE The current sample matches the specified term.
- * @retval FALSE The current sample doesn't match the specified term, or an
- *               invalid term was provided.
- *
- * @private
- */
-static gboolean sample_matches(uint8_t old_sample, uint8_t sample, struct srd_term *term)
-{
-	/* Caller ensures term != NULL. */
-
-	switch (term->type) {
-	case SRD_TERM_HIGH:
-		if (sample == 1)
-			return TRUE;
-		break;
-	case SRD_TERM_LOW:
-		if (sample == 0)
-			return TRUE;
-		break;
-	case SRD_TERM_RISING_EDGE:
-		if (old_sample == 0 && sample == 1)
-			return TRUE;
-		break;
-	case SRD_TERM_FALLING_EDGE:
-		if (old_sample == 1 && sample == 0)
-			return TRUE;
-		break;
-	case SRD_TERM_EITHER_EDGE:
-		if ((old_sample == 1 && sample == 0) || (old_sample == 0 && sample == 1))
-			return TRUE;
-		break;
-	case SRD_TERM_NO_EDGE:
-		if ((old_sample == 0 && sample == 0) || (old_sample == 1 && sample == 1))
-			return TRUE;
-		break;
-	case SRD_TERM_SKIP:
-		if (term->num_samples_already_skipped == term->num_samples_to_skip)
-			return TRUE;
-		term->num_samples_already_skipped++;
-		break;
-	default:
-		srd_err("Unknown term type %d.", term->type);
-		break;
-	}
-
-	return FALSE;
-}
-
 static gboolean have_non_null_conds(const struct srd_decoder_inst *di)
 {
 	GSList *l, *cond;
@@ -138,65 +80,231 @@ static void update_old_pins_array_initial_pins(struct srd_decoder_inst *di)
 	}
 }
 
-static gboolean term_matches(const struct srd_decoder_inst *di,
-		struct srd_term *term, const uint8_t *sample_pos)
-{
-	uint8_t old_sample, sample;
-	int byte_offset, bit_offset, ch;
+static gboolean match_skip_terms(struct srd_decoder_inst *di,
+	const uint64_t num_samples_processed) {
+	const GSList *l, *m;
+	gboolean only_skip, all_skips_complete;
+	for (l = di->condition_list; l; l = l->next) {
+		all_skips_complete = TRUE;
+		only_skip = TRUE;
+		for (m = l->data; m; m = m->next) {
+			struct srd_term *const term = m->data;
+			if (term->type == SRD_TERM_SKIP) {
+				term->num_samples_already_skipped +=
+					num_samples_processed;
+				if (term->num_samples_already_skipped <
+					term->num_samples_to_skip)
+					all_skips_complete = FALSE;
+			} else {
+				only_skip = FALSE;
+			}
 
-	/* Caller ensures di, di->dec_channelmap, term, sample_pos != NULL. */
+		}
 
-	if (term->type == SRD_TERM_SKIP)
-		return sample_matches(0, 0, term);
-
-	ch = term->channel;
-	byte_offset = di->dec_channelmap[ch] / 8;
-	bit_offset = di->dec_channelmap[ch] % 8;
-	sample = *(sample_pos + byte_offset) & (1 << bit_offset) ? 1 : 0;
-	old_sample = di->old_pins_array->data[ch];
-
-	return sample_matches(old_sample, sample, term);
-}
-
-static gboolean all_terms_match(const struct srd_decoder_inst *di,
-		const GSList *cond, const uint8_t *sample_pos)
-{
-	const GSList *l;
-	struct srd_term *term;
-
-	/* Caller ensures di, cond, sample_pos != NULL. */
-
-	for (l = cond; l; l = l->next) {
-		term = l->data;
-		if (!term_matches(di, term, sample_pos))
-			return FALSE;
-	}
-
-	return TRUE;
-}
-
-static gboolean at_least_one_condition_matched(
-		const struct srd_decoder_inst *di, unsigned int num_conditions)
-{
-	unsigned int i;
-
-	/* Caller ensures di != NULL. */
-
-	for (i = 0; i < num_conditions; i++) {
-		if (di->match_array->data[i])
+		if (only_skip && all_skips_complete)
 			return TRUE;
 	}
 
 	return FALSE;
 }
 
+#if defined(HAVE_ALIGNED_ACCESS_REQUIRED) && !defined(WORDS_BIGENDIAN)
+#else
+// SORT THIS OUT!!!
+#endif
+
+#define define_find_match_func(width, sample_type) 				\
+										\
+struct condition_masks_##width {						\
+	sample_type high, low, change, no_change;				\
+};										\
+										\
+static sample_type unpack_sample_##width(const uint8_t *ptr)			\
+{										\
+	return *(sample_type*)ptr;						\
+}										\
+										\
+static void make_condition_masks_##width(struct srd_decoder_inst *const di,	\
+	struct condition_masks_##width *const condition_masks_array,		\
+	size_t *const condition_nums_array,					\
+	uint64_t num_samples_to_process,					\
+	size_t *const num_active_conditions,					\
+	uint64_t *const num_applicable_samples)					\
+{										\
+	unsigned int condition_num;						\
+	const GSList *l, *m;							\
+	*num_active_conditions = 0;						\
+	*num_applicable_samples = num_samples_to_process;			\
+	for (l = di->condition_list, condition_num = 0; l;			\
+		l = l->next, condition_num++) {					\
+		struct condition_masks_##width *const masks =			\
+			condition_masks_array + *num_active_conditions;		\
+		for (m = l->data; m; m = m->next) {				\
+			const struct srd_term *const term = m->data;		\
+										\
+			if (term->type == SRD_TERM_SKIP) {			\
+				const uint64_t num_remaining_to_skip =		\
+					term->num_samples_to_skip -		\
+					term->num_samples_already_skipped;	\
+				if (num_remaining_to_skip <			\
+					*num_applicable_samples)		\
+					*num_applicable_samples =		\
+						num_remaining_to_skip;		\
+				continue;					\
+			}							\
+										\
+			const sample_type bit = 1ULL <<				\
+				di->dec_channelmap[term->channel];		\
+										\
+			if (term->type == SRD_TERM_HIGH ||			\
+				term->type == SRD_TERM_RISING_EDGE)		\
+				masks->high |= bit;				\
+			if (term->type == SRD_TERM_LOW ||			\
+				term->type == SRD_TERM_FALLING_EDGE)		\
+				masks->low |= bit;				\
+			if (term->type == SRD_TERM_RISING_EDGE ||		\
+				term->type == SRD_TERM_FALLING_EDGE ||		\
+				term->type == SRD_TERM_EITHER_EDGE)		\
+				masks->change |= bit;				\
+			if (term->type == SRD_TERM_NO_EDGE)			\
+				masks->no_change |= bit;			\
+		}								\
+										\
+		if (masks->high || masks->low ||				\
+			masks->change || masks->no_change) {			\
+			condition_nums_array[*num_active_conditions] =		\
+				condition_num;					\
+			(*num_active_conditions)++;				\
+		}								\
+	}									\
+}										\
+										\
+static gboolean match_condition_##width(					\
+	const struct condition_masks_##width *const masks,			\
+	const sample_type sample, const sample_type change)			\
+{										\
+	return (sample & masks->high) == masks->high &&				\
+		(~sample & masks->low) == masks->low &&				\
+		(change & masks->change) == masks->change &&			\
+		(~change & masks->no_change) == masks->no_change;		\
+}										\
+										\
+static gboolean quick_search_matches_##width(					\
+	const struct condition_masks_##width *const masks,			\
+	const unsigned int num_conditions, const uint8_t **const sample_pos,	\
+	const size_t num_samples, const size_t unitsize,			\
+	sample_type *prev_sample, sample_type *sample, sample_type *change)	\
+{										\
+	unsigned int i;								\
+	const uint8_t *const end_sample_pos = *sample_pos +			\
+		num_samples * unitsize;						\
+	while (*sample_pos < end_sample_pos) {					\
+		*prev_sample = *sample;						\
+		*sample = unpack_sample_##width(*sample_pos);			\
+		*sample_pos += unitsize;					\
+		*change = *sample ^ *prev_sample;				\
+										\
+		for (i = 0; i != num_conditions; i++)				\
+			if (G_UNLIKELY (match_condition_##width(		\
+				masks + i, *sample, *change)))			\
+				return TRUE;					\
+	}									\
+										\
+	return FALSE;								\
+}										\
+										\
+static gboolean find_match_##width(struct srd_decoder_inst *di)			\
+{										\
+	gboolean any_match = FALSE;						\
+	size_t num_active_conditions;						\
+	uint64_t num_applicable_samples;					\
+	uint64_t num_samples_to_process =					\
+		di->abs_end_samplenum - di->abs_cur_samplenum;			\
+	sample_type prev_sample = 0, sample = 0, change = 0;			\
+	unsigned int i;								\
+										\
+	const unsigned int unitsize = di->data_unitsize;			\
+	const unsigned int num_conditions = g_slist_length(di->condition_list);	\
+	const uint8_t *sample_pos = di->inbuf + unitsize *			\
+		(di->abs_cur_samplenum - di->abs_start_samplenum);		\
+										\
+	/* Allocate mask buffers. */						\
+	struct condition_masks_##width *const masks =				\
+		g_malloc0_n(num_conditions,					\
+			sizeof(struct condition_masks_##width));		\
+	size_t *const mask_condition_nums =					\
+		g_malloc_n(num_conditions, sizeof(size_t));			\
+										\
+	/* di->match_array is NULL here. Create a new GArray. */		\
+	di->match_array = g_array_sized_new(					\
+		FALSE, TRUE, sizeof(gboolean), num_conditions);			\
+	g_array_set_size(di->match_array, num_conditions);			\
+										\
+	/* Sample 0: Set di->old_pins_array for SRD_INITIAL_PIN_SAME_AS_SAMPLE0 \
+	 * pins. */								\
+	if (di->abs_cur_samplenum == 0)						\
+		update_old_pins_array_initial_pins(di);				\
+										\
+	for (i = 0; i != di->dec_num_channels; i++)				\
+		if (di->old_pins_array->data[i])				\
+			sample |= 1ULL << di->dec_channelmap[i];		\
+										\
+	while (num_samples_to_process && !any_match) {				\
+		make_condition_masks_##width(di, masks, mask_condition_nums,	\
+			num_samples_to_process, &num_active_conditions,		\
+			&num_applicable_samples);				\
+										\
+		const uint8_t *const start_sample_pos = sample_pos;		\
+		if (num_active_conditions) {					\
+			/* Search for any matching conditions */		\
+			any_match = quick_search_matches_##width(masks,		\
+				num_conditions,	&sample_pos,			\
+				num_applicable_samples, unitsize,		\
+				&prev_sample, &sample, &change);		\
+		} else if (num_applicable_samples) {				\
+			/* If there are no active masks i.e. only skip		\
+			 * conditions, skip ahead by pointer arithmetic, and	\
+			 * update the the sample words on the final samples.	\
+			 */							\
+			sample_pos += (num_applicable_samples - 1) * unitsize;	\
+			prev_sample = unpack_sample_##width(sample_pos);	\
+			sample_pos += unitsize;					\
+			sample = unpack_sample_##width(sample_pos);		\
+			change = sample ^ prev_sample;				\
+		}								\
+										\
+		const uint64_t num_samples_processed =				\
+			(sample_pos - start_sample_pos) / unitsize;		\
+		num_samples_to_process -= num_samples_processed;		\
+		any_match |= match_skip_terms(di, num_samples_processed);	\
+	}									\
+										\
+	/* If any matched recalculate the results for the match array */	\
+	if (any_match) {							\
+		for (i = 0; i != num_active_conditions; i++) {			\
+			di->match_array->data[mask_condition_nums[i]] =		\
+				match_condition_##width(			\
+					masks + i, sample, change);		\
+		}								\
+	}									\
+										\
+	g_free(masks);								\
+	g_free(mask_condition_nums);						\
+										\
+	di->abs_cur_samplenum = (sample_pos - di->inbuf) / unitsize +		\
+		di->abs_start_samplenum;					\
+	update_old_pins_array(di, sample_pos);					\
+										\
+	return any_match;							\
+}
+
+define_find_match_func(8, uint8_t)
+define_find_match_func(16, uint16_t)
+define_find_match_func(32, uint32_t)
+define_find_match_func(64, uint64_t)
+
 static gboolean find_match(struct srd_decoder_inst *di)
 {
-	uint64_t i, j, num_samples_to_process;
-	GSList *l, *cond;
-	const uint8_t *sample_pos;
-	unsigned int num_conditions;
-
 	/* Caller ensures di != NULL. */
 
 	/* Check whether the condition list is NULL/empty. */
@@ -211,39 +319,14 @@ static gboolean find_match(struct srd_decoder_inst *di)
 		return TRUE;
 	}
 
-	num_samples_to_process = di->abs_end_samplenum - di->abs_cur_samplenum;
-	num_conditions = g_slist_length(di->condition_list);
-
-	/* di->match_array is NULL here. Create a new GArray. */
-	di->match_array = g_array_sized_new(FALSE, TRUE, sizeof(gboolean), num_conditions);
-	g_array_set_size(di->match_array, num_conditions);
-
-	/* Sample 0: Set di->old_pins_array for SRD_INITIAL_PIN_SAME_AS_SAMPLE0 pins. */
-	if (di->abs_cur_samplenum == 0)
-		update_old_pins_array_initial_pins(di);
-
-	for (i = 0; i < num_samples_to_process; i++, (di->abs_cur_samplenum)++) {
-
-		sample_pos = di->inbuf + ((di->abs_cur_samplenum - di->abs_start_samplenum) * di->data_unitsize);
-
-		/* Check whether the current sample matches at least one of the conditions (logical OR). */
-		/* IMPORTANT: We need to check all conditions, even if there was a match already! */
-		for (l = di->condition_list, j = 0; l; l = l->next, j++) {
-			cond = l->data;
-			if (!cond)
-				continue;
-			/* All terms in 'cond' must match (logical AND). */
-			di->match_array->data[j] = all_terms_match(di, cond, sample_pos);
-		}
-
-		update_old_pins_array(di, sample_pos);
-
-		/* If at least one condition matched we're done. */
-		if (at_least_one_condition_matched(di, num_conditions))
-			return TRUE;
+	switch (di->data_unitsize) {
+	case 1: return find_match_8(di);
+	case 2: return find_match_16(di);
+	case 4: return find_match_32(di);
+	case 8: return find_match_64(di);
+	default:
+		assert(FALSE);
 	}
-
-	return FALSE;
 }
 
 /**
@@ -273,10 +356,7 @@ SRD_PRIV int process_samples_until_condition_match(struct srd_decoder_inst *di, 
 		return SRD_OK;
 
 	/* Check if any of the current condition(s) match. */
-	while (TRUE) {
-		/* Feed the (next chunk of the) buffer to find_match(). */
-		*found_match = find_match(di);
-
+	while (!*found_match) {
 		/* Did we handle all samples yet? */
 		if (di->abs_cur_samplenum >= di->abs_end_samplenum) {
 			srd_dbg("Done, handled all samples (abs cur %" PRIu64
@@ -285,14 +365,9 @@ SRD_PRIV int process_samples_until_condition_match(struct srd_decoder_inst *di, 
 			return SRD_OK;
 		}
 
-		/* If we didn't find a match, continue looking. */
-		if (!(*found_match))
-			continue;
-
-		/* At least one condition matched, return. */
-		return SRD_OK;
+		/* Feed the (next chunk of the) buffer to find_match(). */
+		*found_match = find_match(di);
 	}
 
 	return SRD_OK;
 }
-
